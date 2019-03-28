2dbddc998bf5be8175b9de1e416a13727fb28709
Nathan Campos <nathan@innoveworkshop.com>
2019-03-28 11:26:24 +0000

    Added a little documentation to bin/generate_site.pl

--- bin/generate_site.pl
+++ bin/generate_site.pl
@@ -28,3 +28,26 @@ print colored('Generating pages...', 'blue') . "\n";
 Utils::Index::create_pages($config, @posts);
 Utils::Posts::create_pages($config, @posts);
 
+__END__
+
+=head1 NAME
+
+generate_site.pl - The main levissimo blog generator.
+
+=head1 DESCRIPTION
+
+The main executable to generate the levissimo static pages. This executable must
+be use in conjunction with others to work properly. Please take a look at the
+C<Makefile> at the root of the project to get an idea of how this fits into the
+whole system.
+
+=head1 AUTHOR
+
+Nathan Campos <nathanpc@dreamintech.net>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2019 Nathan Campos.
+
+=cut
+
